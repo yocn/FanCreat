99a2cd33127f34f293a59e5620b862a0bf992bac
yocn <539654997@qq.com>
2016-04-07 11:49:51 +0800

    fork test

--- FanCreat/app/src/main/java/com/fantest/wanghui/fan/base/BaseApplication.java
+++ FanCreat/app/src/main/java/com/fantest/wanghui/fan/base/BaseApplication.java
@@ -1,10 +1,13 @@
 package com.fantest.wanghui.fan.base;
 
 import android.app.Application;
+import android.app.Activity;
+import java.util.List;
 
 /**
  * Created by wanghui on 2016/3/29.
  */
 public class BaseApplication extends Application {
+	private List<Activity> activityList;
 
 }
